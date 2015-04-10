4a04976628681bc967db8389d1b3920d303091cb
zts1993 <zts1993@gmail.com>
2015-04-10 15:47:58 +0800

    Initial commit

---
+++ CSE_Frontend/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from CSE_Frontend import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('cse.views',
+
+    # Examples:
+    # url(r'^$', 'CSE_Frontend.views.home', name='home'),
+    # url(r'^CSE_Frontend/', include('CSE_Frontend.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+
+
+    url(r'^$', 'home', name='home'),
+
+    url(r'^q/$', 'q', name='q'),
+
+    url(r'^query/(?P<key>(.*))$', 'query', name='query'),
+
+)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ cse/views.py
@@ -0,0 +1,55 @@
+# Create your views here.
+import random
+import urllib
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+import requests, json
+from CSE_Frontend import settings
+
+
+def urlencode(str):
+    reprStr = repr(str).replace(r'\x', '%')
+    return reprStr[1:-1]
+
+
+def _get_index_api():
+    servers = settings.CSE_BACKEND_ENDPOINT
+    random.shuffle(servers)
+    return servers[0]
+
+
+def home(request):
+    return render_to_response('home.html', context_instance=RequestContext(request))
+
+
+def query(request, key):
+    p = request.GET.get('p', '1')
+    print p
+    url = _get_index_api() + urllib.quote(key.encode("utf-8").replace("\\", "%2f"))
+
+    params = {"p": p, }
+    r = requests.get(url, params=params)
+    print r.url, r.status_code
+
+    json_res = r.json()
+
+    keyWord = json_res['keyWord'].replace("%2f", "\\")
+    pager = json_res['pager']
+    queryResultItems = json_res['queryResultItems']
+
+    empty_counter = range(1, pager['totalPage'])
+    #print json_res
+
+    return render_to_response('query.html', {'keyWord': keyWord, 'pager': pager, 'empty_counter': empty_counter,
+                                             'queryResultItems': queryResultItems},
+                              context_instance=RequestContext(request))
+
+
+def q(request):
+    if request.method == 'POST':
+        key = request.POST['keyWords']
+
+        return HttpResponseRedirect(reverse('query', args={key}))
+    return HttpResponse("error visit method")
